da81dbaa1552f053a033c04cc468fb7a2ee6f184
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-7.42heilbronn.de>
2025-04-06 18:32:15 +0200

    Finalizing the init classes

--- ex00/Cat.cpp
+++ ex00/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 18:22:28 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/06 18:22:52 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/06 18:29:58 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,22 @@ Cat::Cat() : Animal()
 Cat::Cat(const Cat& other)
 {
     std::cout << "Cat copy constructor called" << std::endl;
+}
+
+Cat& Cat::operator=(const Cat &other)
+{
+    if (this != &other)
+        Animal::operator=(other);
+    std::cout << "Cat assignment operator called" << std::endl;
+    return *this;
+}
+
+Cat::~Cat()
+{
+    std::cout << "Cat deconstructor called" << std::endl;
+}
+
+void    Cat::makeSound() const
+{
+    std::cout << "Meow!" << std::endl;
 }
