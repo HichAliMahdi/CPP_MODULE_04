21e4b92174d648ccb7d0401704bb1ebbb5402e00
Hichem Abderrahmane Ali Mahdi <hali-mah@2-G-9.42heilbronn.de>
2025-04-07 14:03:40 +0200

    added the brain.cpp class

---
+++ ex01/Brain.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 13:57:22 by hali-mah          #+#    #+#             */
+/*   Updated: 2025/04/07 14:03:24 by hali-mah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Brain.hpp"
+
+Brain::Brain()
+{
+    std::cout << "Brain constructor called" << std::endl;
+    for (int i = 0, i < 100, i++)
+        ideas[i] = "";
+}
+
+Brain::~Brain()
+{
+    std::cout << "Brain deconstructor called" << std::endl;
+}
+
+Brain::Brain(const Brain& other)
+{
+    std::cout << "Brain copy constructor called" << std::endl;
+    for (int i, i < 100, i++)
+        this->ideas[i] = other.ideas[i];
+}
+
+Brain& Brain::operator=(const Brain& other)
+{
+    std::cout << "Brain assignment operator called" << std::endl;
+    if (this != other)
+    {
+        for (int i = 0; i < 100; i++)
+            this->ideas[i] = other.ideas[i];
+    }
+    return *this;
+}
+
+void Brain::setIdea(int index, const std::string& idea)
+{
+    if (index >= 0 && index < 100)
+        ideas[index] = idea;
+}
+
+std::string Brain::getIdea(int index) const
+{
+    if (index >= 0 && index < 100)
+        return ideas[index];
+    return "";
+}
