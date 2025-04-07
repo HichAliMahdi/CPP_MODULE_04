47d555f59f56e867987f77443d7234920c9c6a35
Hichem Abderrahmane Ali Mahdi <hali-mah@2-G-9.42heilbronn.de>
2025-04-07 14:08:59 +0200

    Updated dog and cat functions with brain

--- ex01/Cat.cpp
+++ ex01/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 18:22:28 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/06 18:29:58 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/07 14:08:40 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,31 @@
 Cat::Cat() : Animal()
 {
     this->type = "Cat";
+    this->brain = new Brain();
     std::cout << "Cat constructor called" << std::endl;
 }
 
 Cat::Cat(const Cat& other)
 {
     std::cout << "Cat copy constructor called" << std::endl;
+    this->brain = new Brain(*(other.brain));
 }
 
 Cat& Cat::operator=(const Cat &other)
 {
-    if (this != &other)
-        Animal::operator=(other);
     std::cout << "Cat assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        Animal::operator=(other);
+        delete this->brain;
+        this->brain = new Brain(*(other.brain));
+    }
     return *this;
 }
 
 Cat::~Cat()
 {
+    delete this->brain;
     std::cout << "Cat deconstructor called" << std::endl;
 }
 
--- ex01/Dog.cpp
+++ ex01/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 18:22:58 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/07 14:05:33 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/07 14:07:18 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,31 @@
 Dog::Dog() : Animal()
 {
     this->type = "Dog";
-    this->brain = 
+    this->brain = new Brain();
     std::cout << "Dog constructor called" << std::endl;
 }
 
 Dog::Dog(const Dog& other) : Animal(other)
 {
     std::cout << "Dog copy constructor called" << std::endl;
+    this->brain = new Brain(*(other.brain));
 }
 
 Dog& Dog::operator=(const Dog& other)
 {
-    if (this != &other)
-        Animal::operator=(other);
     std::cout << "Dog assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        Animal::operator=(other);
+        delete this->brain;
+        this->brain = new Brain(*(other.brain));
+    }
     return *this;
 }
 
 Dog::~Dog()
 {
+    delete this->brain;
     std::cout << "Dog deconstructor called" << std::endl;
 }
 
